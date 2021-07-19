296857c95215c3dda859db06f37dc895789cb23d
ykysg <56275164+ykysg@users.noreply.github.com>
2021-07-20 07:25:10 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-   
-    let targetImage = document.querySelector("#smart-image");
-    targetImage.addEventListener("click", function() {
-
-        targetImage.classList.remove("small");
-    });
-    
-});
+var lang=document.getElementById('lang');
+    document.getElementById('btn').onclick=function(){
+        var cont=document.createElement('div');
+        cont.innerHTML='<p>'+lang.value+'</p><hr>';
+        document.getElementById('div1').appendChild(cont);
+        lang.value='';
+    }
